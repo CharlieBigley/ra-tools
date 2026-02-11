e394ce59b0821293ca7a063406c81b7729d04153
Charlie Bigley <charlie@bigley.me.uk>
2026-02-11 19:40:59 +0000

    Refactor dashboard: block-based structure with 2-column grid layout
    - Restructure template to use data-driven block structure
    - Permission checks applied at block construction time
    - Two-column responsive grid layout using CSS Grid
    - Clean separation of logic (blocks) and presentation (rendering)
    - Consistent styling for all dashboard blocks
    - Add dashboard.css with responsive breakpoints
    - Maintains all existing functionality and permissions

--- com_ra_tools/administrator/tmpl/dashboard/default.php
+++ com_ra_tools/administrator/tmpl/dashboard/default.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * @version     3.4.6
+ * @version     3.5.0
  * @package     com_ra_tools
  * @copyright   Copyleft (C) 2021
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
@@ -21,11 +21,11 @@
  * 14/11/25 CB Mailman recipients
  * 02/02/26 CB add Clusters
  * 04/02/26 CB Add RA Develop section
+ * 11/02/26 CB Restructure with grid layout and permission-based blocks
  */
 // No direct access
 \defined('_JEXEC') or die;
 
-//use \Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Component\ComponentHelper;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Helper\ContentHelper;
@@ -33,62 +33,127 @@ use Ramblers\Component\Ra_events\Site\Helpers\EventsHelper;
 use Ramblers\Component\Ra_tools\Site\Helpers\JsonHelper;
 use Ramblers\Component\Ra_tools\Site\Helpers\ToolsHelper;
 
-//use Ramblers\Component\Ra_tools\Site\Helpers\ToolsTable;
-//JHtml::_('behavior.tooltip');
 $jsonHelper = new JsonHelper;
 $toolsHelper = new ToolsHelper;
 
 $wa = $this->document->getWebAssetManager();
 $wa->registerAndUseStyle('ramblers', 'com_ra_tools/ramblers.css');
+$wa->registerAndUseStyle('dashboard', 'com_ra_tools/dashboard.css');
 
 $component = ComponentHelper::getComponent('com_ra_tools');
 $canDo = ContentHelper::getActions('com_ra_tools');
 
-//echo '<div style="float: left">';     // Div for Org & logo
-//echo $toolsHelper->showLogo();
-//
+// Build blocks array
+$blocks = [];
 
+// ========== SYSTEM TOOLS BLOCK (SUPERUSER ONLY) ==========
 if ($toolsHelper->isSuperuser()) {
-echo '<div style="float: right">';
-echo '<h3>RA Develop</h3>';
-echo '<ul>';
-echo '<li><a href="index.php?option=com_ra_develop&task=extensions.listExtension" target="_self">Summary</a></li>' . PHP_EOL;
-
-//    if ((ComponentHelper::isEnabled('com_ra_develop', true))) {
-        echo '<li><a href="index.php?option=com_ra_develop&view=builds" target="_self">List Builds</a></li>' . PHP_EOL;
-//    }
-    echo '<li><a href="index.php?option=com_ra_develop&view=subsystems" target="_self">Sub systems</a></li>' . PHP_EOL;
-    echo '<li><a href="index.php?option=com_ra_develop&view=extensions" target="_self">Extensions</a></li>' . PHP_EOL;
-    echo '<li><a href="index.php?option=com_ra_develop&view=extension_types" target="_self">Extension Types</a></li>' . PHP_EOL;
-    echo '</div>' . PHP_EOL;
+    $sysToolsItems = [
+        ['label' => 'Show your access permissions', 'url' => 'index.php?option=com_ra_tools&task=system.showAccess'],
+    ];
+    
+    if ((ComponentHelper::isEnabled('com_ra_events', true)) || (ComponentHelper::isEnabled('com_ra_mailman', true))) {
+        $sysToolsItems[] = ['label' => 'List Users', 'url' => 'index.php?option=com_ra_tools&view=users'];
     }
-
-if ($toolsHelper->isSuperuser()) {
-    echo '<div style="float: right">';
-    echo '<h3>System tools</h3>';
-    echo '<ul>';
-    echo '<li><a href="index.php?option=com_ra_tools&task=system.showAccess" target="_self">Show your access permissions</a></li>' . PHP_EOL;
-
-    if ((ComponentHelper::isEnabled('com_ra_events', true)) OR (ComponentHelper::isEnabled('com_ra_mailman', true))) {
-        echo '<li><a href="index.php?option=com_ra_tools&view=users" target="_self">List Users</a></li>' . PHP_EOL;
-    }
-    echo '<li><a href="index.php?option=com_ra_tools&view=apisites" target="_self">API sites</a></li>' . PHP_EOL;
-    echo '<li><a href="index.php?option=com_ra_tools&task=system.AccessWizard" target="_self">Access Configuration Wizard</a></li>' . PHP_EOL;
-    echo '<li><a href="index.php?option=com_ra_tools&view=reports" target="_self">System Reports</a></li>' . PHP_EOL;
-//    echo 'Orig  https://walks-manager.ramblers.org.uk/api/volunteers/groups?api-key=742d93e8f409bf2b5aec6f64cf6f405e<br>';
+    
+    $sysToolsItems[] = ['label' => 'API sites', 'url' => 'index.php?option=com_ra_tools&view=apisites'];
+    $sysToolsItems[] = ['label' => 'Access Configuration Wizard', 'url' => 'index.php?option=com_ra_tools&task=system.AccessWizard'];
+    $sysToolsItems[] = ['label' => 'System Reports', 'url' => 'index.php?option=com_ra_tools&view=reports'];
+    
     $target = $jsonHelper->setUrl('organisation', '');
-    echo '<li><a href="' . $target . '"';
-    echo ' target="_blank">Show Organisation feed</a></li>' . PHP_EOL;
-    echo '<li><a href="index.php?option=com_ra_tools&amp;task=area_list.refreshAreas" target="_self">Refresh details of Areas</a></li>' . PHP_EOL;
-    echo '<li><a href="index.php?option=com_ra_tools&amp;task=group_list.refreshGroups" target="_self">Refresh details of Groups</a></li>' . PHP_EOL;
-    echo '</div>' . PHP_EOL;
+    $sysToolsItems[] = ['label' => 'Show Organisation feed', 'url' => $target, 'target' => '_blank'];
+    
+    $sysToolsItems[] = ['label' => 'Refresh details of Areas', 'url' => 'index.php?option=com_ra_tools&task=area_list.refreshAreas'];
+    $sysToolsItems[] = ['label' => 'Refresh details of Groups', 'url' => 'index.php?option=com_ra_tools&task=group_list.refreshGroups'];
+    
+    if ($canDo->get('core.admin')) {
+        $versions = $toolsHelper->getVersions('com_ra_tools');
+        $sysToolsItems[] = ['label' => 'Configure com_ra_tools (v' . $versions->component . ')', 'url' => 'index.php?option=com_config&view=component&component=com_ra_tools'];
+        $sysToolsItems[] = ['label' => 'DB version: ' . $versions->db_version, 'url' => '#', 'disabled' => true];
     }
+    
+    $blocks[] = [
+        'title' => 'System Tools',
+        'items' => $sysToolsItems
+    ];
+}
 
-if ($canDo->get('core.admin')) {
-    $versions = $toolsHelper->getVersions('com_ra_tools');
-    echo '<li><a href="index.php?option=com_config&view=component&component=com_ra_tools" target="_self">';
-    echo "Configure com_ra_tools (version " . $versions->component . ")</a></li>" . PHP_EOL;
-    echo '<li>(DB version is ' . $versions->db_version . ')</li>';
+
+
+
+// ========== MAIL MANAGER BLOCK ==========
+if (ComponentHelper::isEnabled('com_ra_mailman', true)) {
+    $mailmanCanDo = ContentHelper::getActions('com_ra_mailman');
+    
+    $mailmanItems = [
+        ['label' => 'Mailing lists', 'url' => 'index.php?option=com_ra_mailman&view=mail_lsts'],
+        ['label' => 'Mailshots', 'url' => 'index.php?option=com_ra_mailman&view=mailshots'],
+    ];
+    
+    if ($mailmanCanDo->get('core.create')) {
+        $mailmanItems[] = ['label' => 'Subscriptions', 'url' => 'index.php?option=com_ra_mailman&view=subscriptions'];
+        $mailmanItems[] = ['label' => 'Recipients', 'url' => 'index.php?option=com_ra_mailman&view=recipients'];
+        $mailmanItems[] = ['label' => 'MailMan Users', 'url' => 'index.php?option=com_ra_mailman&view=profiles'];
+        $mailmanItems[] = ['label' => 'Import list of members', 'url' => 'index.php?option=com_ra_mailman&view=dataload'];
+        $mailmanItems[] = ['label' => 'Import Reports', 'url' => 'index.php?option=com_ra_mailman&view=import_reports'];
+        $mailmanItems[] = ['label' => 'Mailman Reports', 'url' => 'index.php?option=com_ra_mailman&view=reports'];
+        $mailmanItems[] = ['label' => 'Process Renewals', 'url' => 'index.php?option=com_ra_mailman&task=system.checkRenewals'];
+    }
+    
+    if ($mailmanCanDo->get('core.admin')) {
+        $versions = $toolsHelper->getVersions('com_ra_mailman');
+        $mailmanItems[] = ['label' => 'Configure com_ra_mailman (v' . $versions->component . ')', 'url' => 'index.php?option=com_config&view=component&component=com_ra_mailman'];
+        $mailmanItems[] = ['label' => 'DB version: ' . $versions->db_version, 'url' => '#', 'disabled' => true];
+    }
+    
+    $blocks[] = [
+        'title' => 'Mail Manager',
+        'items' => $mailmanItems
+    ];
+}
+
+// ========== ORGANISATION BLOCK ==========
+$orgItems = [
+    ['label' => 'List of Clusters', 'url' => 'index.php?option=com_ra_tools&view=clusters'],
+    ['label' => 'List of Areas', 'url' => 'index.php?option=com_ra_tools&view=area_list'],
+    ['label' => 'List of Groups', 'url' => 'index.php?option=com_ra_tools&view=group_list'],
+];
+
+$blocks[] = [
+    'title' => 'Organisation',
+    'items' => $orgItems
+];
+
+// ========== PATH MAINTENANCE BLOCK ==========
+if (ComponentHelper::isEnabled('com_ra_paths', true)) {
+    $pathsCanDo = ContentHelper::getActions('com_ra_paths');
+    
+    $pathsItems = [
+        ['label' => 'Fault Reports', 'url' => 'index.php?option=com_ra_paths&view=faults'],
+        ['label' => 'Followups', 'url' => 'index.php?option=com_ra_paths&view=followups'],
+    ];
+    
+    if ($pathsCanDo->get('core.create')) {
+        $pathsItems[] = ['label' => 'Categories', 'url' => 'index.php?option=com_ra_paths&view=categories'];
+        $pathsItems[] = ['label' => 'Statuses', 'url' => 'index.php?option=com_ra_paths&view=statuses'];
+        $pathsItems[] = ['label' => 'Parishes', 'url' => 'index.php?option=com_ra_paths&view=parishes'];
+        $pathsItems[] = ['label' => 'Boroughs', 'url' => 'index.php?option=com_ra_paths&view=boroughs'];
+        $pathsItems[] = ['label' => 'Regions', 'url' => 'index.php?option=com_ra_paths&view=regions'];
+        $pathsItems[] = ['label' => 'Sectors', 'url' => 'index.php?option=com_ra_paths&view=sectors'];
+    }
+    
+    $pathsItems[] = ['label' => 'Enhancements', 'url' => 'index.php?option=com_ra_paths&view=enhancements'];
+    
+    if ($pathsCanDo->get('core.admin')) {
+        $versions = $toolsHelper->getVersions('com_ra_paths');
+        $pathsItems[] = ['label' => 'Configure com_ra_paths (v' . $versions->component . ')', 'url' => 'index.php?option=com_config&view=component&component=com_ra_paths'];
+        $pathsItems[] = ['label' => 'DB version: ' . $versions->db_version, 'url' => '#', 'disabled' => true];
+    }
+    
+    $blocks[] = [
+        'title' => 'Path Maintenance',
+        'items' => $pathsItems
+    ];
 }
 
 echo '</ul>';
@@ -154,6 +219,147 @@ if (ComponentHelper::isEnabled('com_ra_paths', true)) {
     echo '</ul>' . PHP_EOL;
 }
 
+
+
+// ========== EVENTS BLOCK ==========
+if (ComponentHelper::isEnabled('com_ra_events', true)) {
+    $versions = $toolsHelper->getVersions('com_ra_events');
+    
+    // For newer versions, use EventsHelper
+    if (version_compare($versions->component, '2.1.1', 'ge')) {
+        $eventsHelper = new EventsHelper;
+        // EventsHelper handles its own block addition
+        $eventsHelper->menusDashboard();
+    } else {
+        // Fallback for older versions
+        $eventsCanDo = ContentHelper::getActions('com_ra_events');
+        
+        $eventsItems = [
+            ['label' => 'List of Events', 'url' => 'index.php?option=com_ra_events&view=events'],
+        ];
+        
+        if ($eventsCanDo->get('core.create')) {
+            $eventsItems[] = ['label' => 'List of Bookings', 'url' => 'index.php?option=com_ra_events&view=bookings'];
+            $eventsItems[] = ['label' => 'Event Reports', 'url' => 'index.php?option=com_ra_events&view=reports'];
+            $eventsItems[] = ['label' => 'Import list of bookings', 'url' => 'index.php?option=com_ra_events&view=dataload'];
+            $eventsItems[] = ['label' => 'API Sites', 'url' => 'index.php?option=com_ra_tools&view=apisites'];
+        }
+        
+        if ($toolsHelper->isSuperuser()) {
+            $eventsItems[] = ['label' => 'Event Types', 'url' => 'index.php?option=com_ra_events&view=eventtypes'];
+        }
+        
+        if ($eventsCanDo->get('core.admin')) {
+            $versions = $toolsHelper->getVersions('com_ra_events');
+            $eventsItems[] = ['label' => 'Configure com_ra_events (v' . $versions->component . ')', 'url' => 'index.php?option=com_config&view=component&component=com_ra_events'];
+            $eventsItems[] = ['label' => 'DB version: ' . $versions->db_version, 'url' => '#', 'disabled' => true];
+        }
+        
+        $blocks[] = [
+            'title' => 'Events',
+            'items' => $eventsItems
+        ];
+    }
+}
+
+// ========== WALKS BLOCK ==========
+if (ComponentHelper::isEnabled('com_ra_walks', true)) {
+    $walksCanDo = ContentHelper::getActions('com_ra_walks');
+    
+    $walksItems = [
+        ['label' => 'List of Walks', 'url' => 'index.php?option=com_ra_walks&view=walks'],
+        ['label' => 'Walk Reports', 'url' => 'index.php?option=com_ra_walks&view=reports'],
+    ];
+    
+    // Check if walks refresh is available
+    if (ComponentHelper::isEnabled('com_ra_wf', true)) {
+        if (ContentHelper::getActions('com_ra_wf')->get('core.create')) {
+            $walksItems[] = ['label' => 'Refresh details of Walks', 'url' => 'index.php?option=com_ra_wf&task=walks.refresh'];
+        }
+    } else {
+        if ($walksCanDo->get('core.create')) {
+            $walksItems[] = ['label' => 'Refresh details of Walks', 'url' => 'index.php?option=com_ra_walks&task=walks.refresh'];
+        }
+    }
+    
+    if ($walksCanDo->get('core.admin')) {
+        $versions = $toolsHelper->getVersions('com_ra_walks');
+        $walksItems[] = ['label' => 'Configure com_ra_walks (v' . $versions->component . ')', 'url' => 'index.php?option=com_config&view=component&component=com_ra_walks'];
+        $walksItems[] = ['label' => 'DB version: ' . $versions->db_version, 'url' => '#', 'disabled' => true];
+    }
+    
+    $blocks[] = [
+        'title' => 'Walks',
+        'items' => $walksItems
+    ];
+}
+
+// ========== RA DEVELOPMENT BLOCK ==========
+if (ComponentHelper::isEnabled('com_ra_develop', true)) {
+    $developCanDo = ContentHelper::getActions('com_ra_develop');
+    
+    $developItems = [
+        ['label' => 'Summary of extensions', 'url' => 'index.php?option=com_ra_develop&task=extensions.listExtensions'],
+        ['label' => 'Builds', 'url' => 'index.php?option=com_ra_develop&view=builds'],
+        ['label' => 'Sub Systems', 'url' => 'index.php?option=com_ra_develop&view=subsystems'],
+        ['label' => 'Extension Types', 'url' => 'index.php?option=com_ra_develop&view=extension_types'],
+        ['label' => 'Extensions', 'url' => 'index.php?option=com_ra_develop&view=extensions'],
+    ];
+    
+    if ($developCanDo->get('core.admin')) {
+        $versions = $toolsHelper->getVersions('com_ra_develop');
+        $developItems[] = ['label' => 'Configure com_ra_develop (v' . $versions->component . ')', 'url' => 'index.php?option=com_config&view=component&component=com_ra_develop'];
+        $developItems[] = ['label' => 'DB version: ' . $versions->db_version, 'url' => '#', 'disabled' => true];
+    }
+    
+    $blocks[] = [
+        'title' => 'RA Development',
+        'items' => $developItems
+    ];
+}
+
+// ========== WALKS FOLLOW BLOCK ==========
+if (ComponentHelper::isEnabled('com_ra_wf', true)) {
+    $wfCanDo = ContentHelper::getActions('com_ra_wf');
+    
+    $wfItems = [
+        ['label' => 'List of Walks to Follow', 'url' => 'index.php?option=com_ra_wf&view=walks'],
+        ['label' => 'Walks Follow profiles', 'url' => 'index.php?option=com_ra_wf&view=profiles'],
+        ['label' => 'Walks Follow Reports', 'url' => 'index.php?option=com_ra_wf&view=reports'],
+    ];
+    
+    if ($wfCanDo->get('core.admin')) {
+        $versions = $toolsHelper->getVersions('com_ra_wf');
+        $wfItems[] = ['label' => 'Configure com_ra_wf (v' . $versions->component . ')', 'url' => 'index.php?option=com_config&view=component&component=com_ra_wf'];
+        $wfItems[] = ['label' => 'DB version: ' . $versions->db_version, 'url' => '#', 'disabled' => true];
+    }
+    
+    $blocks[] = [
+        'title' => 'Walks Follow',
+        'items' => $wfItems
+    ];
+}
+
+// ========== SELF GUIDED WALKS BLOCK ==========
+if (ComponentHelper::isEnabled('com_ra_sg', true)) {
+    $sgCanDo = ContentHelper::getActions('com_ra_sg');
+    
+    $sgItems = [
+        ['label' => 'Self Guided walks', 'url' => 'index.php?option=com_ra_tools&view=sg_list'],
+        ['label' => 'Categories', 'url' => 'index.php?option=com_categories&extension=com_ra_tools'],
+    ];
+    
+    if ($sgCanDo->get('core.admin')) {
+        $versions = $toolsHelper->getVersions('com_ra_sg');
+        $sgItems[] = ['label' => 'Configure com_ra_sg (v' . $versions->component . ')', 'url' => 'index.php?option=com_config&view=component&component=com_ra_sg'];
+        $sgItems[] = ['label' => 'DB version: ' . $versions->db_version, 'url' => '#', 'disabled' => true];
+    }
+    
+    $blocks[] = [
+        'title' => 'Self Guided Walks',
+        'items' => $sgItems
+    ];
+}
 
 if (ComponentHelper::isEnabled('com_ra_events', true)) {
 //echo '<div style="float: left">';
@@ -264,3 +470,32 @@ if (ComponentHelper::isEnabled('com_ra_sg', true)) {   // Self Guided
     }
     echo '</ul>' . PHP_EOL;
 }
+?>
+
+<!-- Grid-based Dashboard Layout -->
+<div class="dashboard-grid">
+    <?php foreach ($blocks as $block): ?>
+        <div class="dashboard-block">
+            <div class="block-header">
+                <h3><?php echo $block['title']; ?></h3>
+            </div>
+            <div class="block-content">
+                <ul>
+                    <?php foreach ($block['items'] as $item): ?>
+                        <li>
+                            <?php if (isset($item['disabled']) && $item['disabled']): ?>
+                                <span class="item-text"><?php echo $item['label']; ?></span>
+                            <?php else: ?>
+                                <a href="<?php echo $item['url']; ?>" target="<?php echo $item['target'] ?? '_self'; ?>">
+                                    <?php echo $item['label']; ?>
+                                </a>
+                            <?php endif; ?>
+                        </li>
+                    <?php endforeach; ?>
+                </ul>
+            </div>
+        </div>
+    <?php endforeach; ?>
+</div>
+
+
