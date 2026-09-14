9b83785f0c4c4bf3257f65084e3846135fe2ab2f
Charlie Bigley <42494431+CharlieBigley@users.noreply.github.com>
2026-09-14 14:10:54 +0100

    Update ra_tools.php
    First attempt

--- plg_system_ra_tools/ra_tools.php
+++ plg_system_ra_tools/ra_tools.php
@@ -3,10 +3,14 @@
 defined('_JEXEC') or die;
 
 use Joomla\CMS\Plugin\CMSPlugin;
+use Ramblers/Ra_tools/Site/Helpers/ToolsHelper;
 
 class PlgSystemRaTools extends CMSPlugin {
 
-    public function onAfterInitialise(): void {
+    public function onAfterInitialise(): void{
+$toolsHelper = new ToolsHelper;
+$sql = 'DELETE FROM #__ra_profiles WHERE user_id=' . $id;
+$toolsHelper->executeCommand($sql);
     }
 
 }
